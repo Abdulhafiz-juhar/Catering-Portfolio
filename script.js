14e86a05877b599dcbe1d4fd32b87e5eff6d637f
Abdulhafiz-juhar <abdulhafizjuharajmbest@gmail.com>
2024-08-12 15:00:21 +0300

    Make the arrow appear where option is selected

--- script.js
+++ script.js
@@ -47,12 +47,38 @@ businessRight.appendChild(learnMoreDiv);
 Services.appendChild(businessRight);
 
 // options elements
-
 const options = document.querySelectorAll(".option");
 
+// Get the business-left element
+let businessLeftDiv = document.querySelector(".option1");
+// Create the arrow element
+let arrowElement = document.createElement("div");
+arrowElement.classList.add("arrow");
+
+// Set the arrow properties
+arrowElement.style.position = "absolute";
+arrowElement.style.bottom = "40%";
+arrowElement.style.left = "100%";
+arrowElement.style.transform = "translateX(-50%) rotate(315deg)";
+arrowElement.style.width = "0";
+arrowElement.style.height = "0";
+arrowElement.style.borderStyle = "solid";
+arrowElement.style.borderWidth = "0";
+arrowElement.style.borderColor = "transparent";
+arrowElement.style.transition = "border-width 0.3s ease";
+arrowElement.style.borderWidth = "10px 10px 0 0";
+arrowElement.style.borderColor = "white transparent transparent transparent";
+
+// Append the arrow element to the business-left element
+businessLeftDiv.appendChild(arrowElement);
+businessLeftDiv.style.fontWeight = "bold";
+businessLeftDiv.style.color = "white";
 options.forEach((option) => {
   option.addEventListener("click", () => {
     console.log(`Clicked: ${option.classList[1]}`);
+    // Clear the styling applied before to the old selected option
+    businessLeftDiv.style.fontWeight = "";
+    businessLeftDiv.style.color = "";
     // Add your code here for what should happen on click
     // Add classes and other properties
 
@@ -62,6 +88,11 @@ options.forEach((option) => {
       headerDiv.style.opacity = "1";
     }, 200);
 
+    //Make arrow apear and text bold on the currently clicked option
+    businessLeftDiv = document.querySelector(`.${option.classList[1]}`);
+    businessLeftDiv.appendChild(arrowElement);
+    businessLeftDiv.style.fontWeight = "bold";
+    businessLeftDiv.style.color = "white";
     headerDiv.textContent = `${option.children[1].textContent}`;
   });
 });
